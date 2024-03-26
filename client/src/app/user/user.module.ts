71258f16ed6aebfb5c7330813227a114b0304ef7
Lyuboslav03 <lyuboslav00@gmail.com>
2024-03-26 20:59:54 +0200

    Implement user routing

--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
+import { UserRoutingModule } from './user-routing.module';
 
 
 
@@ -9,7 +10,8 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    UserRoutingModule
   ],
   exports: [RegisterComponent]
 })
