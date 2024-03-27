ee79e7838bf17a73d32024b5d86f0937c11886fc
Lyuboslav03 <lyuboslav00@gmail.com>
2024-03-27 23:12:05 +0200

    Add route for add game component

--- client/src/app/games/games-routing.module.ts
+++ client/src/app/games/games-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AllGamesComponent } from "./all-games/all-games.component";
+import { AddGameComponent } from "./add-game/add-game.component";
 
 const routes: Routes = [
     {
         path: 'all-games', children: [
             { path: '', pathMatch: 'full', component: AllGamesComponent }
         ]
+    },
+    {
+        path: 'add-game', component: AddGameComponent
     }
 ]
 
