af522a0fd48aa24d5ca968fbe6972e1c7930f835
Lyuboslav03 <lyuboslav00@gmail.com>
2024-03-27 16:33:32 +0200

    Show dynamic navigation based on user authentication

--- client/src/app/user/user.service.ts
+++ client/src/app/user/user.service.ts
@@ -1,18 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
+import { UserForAuth } from '../types/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor(private http: HttpClient) { }
+  private user$$ = new BehaviorSubject<UserForAuth | undefined>(undefined);
+  private user$ = this.user$$.asObservable();
+
+  user: UserForAuth | undefined;
+
+  get isLoggedIn(): boolean {
+    return !!this.user;
+  }
+
+  constructor(private http: HttpClient) {
+    this.user$.subscribe((user) => {
+      this.user = user;
+    })
+  }
 
   register(username: string, email: string, password: string, rePassword: string) {
-    return this.http.post('/api/register', {
+    return this.http.post<UserForAuth>('/api/register', {
       username,
       email,
       password,
       rePassword
-    })
+    }).pipe(tap((user) => {
+      this.user$$.next(user);
+    }))
   }
 }
