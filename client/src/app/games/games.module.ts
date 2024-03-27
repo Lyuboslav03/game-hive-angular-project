557a7bab4bb1fd868184b6192f0fc29919ad3cc9
Lyuboslav03 <lyuboslav00@gmail.com>
2024-03-27 23:10:20 +0200

    Create add game component

--- client/src/app/games/games.module.ts
+++ client/src/app/games/games.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllGamesComponent } from './all-games/all-games.component';
 import { GamesRoutingModule } from './games-routing.module';
+import { AddGameComponent } from './add-game/add-game.component';
 
 
 
 @NgModule({
   declarations: [
-    AllGamesComponent
+    AllGamesComponent,
+    AddGameComponent
   ],
   imports: [
     CommonModule,
     GamesRoutingModule
   ],
-  exports: [AllGamesComponent]
+  exports: [AllGamesComponent, AddGameComponent]
 })
 export class GamesModule { }
