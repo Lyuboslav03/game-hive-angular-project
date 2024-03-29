2ba5e88abdc13866130cdc7076311f357cd358db
Lyuboslav03 <lyuboslav00@gmail.com>
2024-03-29 20:47:04 +0200

    Add route for current game

--- client/src/app/games/games-routing.module.ts
+++ client/src/app/games/games-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AllGamesComponent } from "./all-games/all-games.component";
 import { AddGameComponent } from "./add-game/add-game.component";
+import { CurrentGameComponent } from "./current-game/current-game.component";
 
 const routes: Routes = [
     {
         path: 'all-games', children: [
-            { path: '', pathMatch: 'full', component: AllGamesComponent }
+            { path: '', pathMatch: 'full', component: AllGamesComponent },
+            { path: ':gameId', component: CurrentGameComponent }
         ]
     },
     {
